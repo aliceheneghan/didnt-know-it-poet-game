1a84104d8d6a63ad8e296e61342dbb6f297e5606
aliceheneghan <heneghan31@gmail.com>
2022-05-23 15:39:54 +0200

    created class PoemTemplate

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ class poemTemplate {
   constructor(number, title, line1, line2, line3, line4, line5) {
     this.number = number;
     this.title = title;
-    this.line1 = line1;
-    this.line2 = line2;
-    this.line3 = line3;
-    this.line4 = line4;
-    this.line5 = line5;
+    this.poem = [line1, line2, line3, line4, line5];
   }
 }
+
+const poem1 = new poemTemplate(1, "New Snack", "This morning I tried a new snack", "I found it at the bottom of ...", "It was ... and yummy", "It felt ... in my tummy", "And now the new snack ...");
+
+console.log(poem1)
