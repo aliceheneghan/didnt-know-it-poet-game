4abcfef08a89da1229d5494479bc8778e11c689b
aliceheneghan <heneghan31@gmail.com>
2022-05-23 15:29:44 +0200

    created class PoemTemplate

---
+++ main.js
@@ -0,0 +1,11 @@
+class poemTemplate {
+  constructor(number, title, line1, line2, line3, line4, line5) {
+    this.number = number;
+    this.title = title;
+    this.line1 = line1;
+    this.line2 = line2;
+    this.line3 = line3;
+    this.line4 = line4;
+    this.line5 = line5;
+  }
+}
