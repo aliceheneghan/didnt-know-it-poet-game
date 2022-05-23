2966921201a70d4ce8d921ed657c688600bd43a5
aliceheneghan <heneghan31@gmail.com>
2022-05-23 21:36:20 +0200

    created play function that generates random number, matches it with poem number and pushes that poem to randomPoem array

--- main.js
+++ main.js
@@ -6,27 +6,28 @@ class PoemTemplate {
     this.title = title;
     this.poem = [line1, line2, line3, line4, line5];
   }
-  play () {
-    
-      if (this.number === selector) {
-
-      }
-  }
 }
 
 const poem1 = new PoemTemplate(1, "New Snack", "This morning I tried a new snack", "I found it at the bottom of ...", "It was ... and yummy", "It felt ... in my tummy", "And now the new snack ...");
 const poem2 = new PoemTemplate(2, "Cocktail Hour", "Have you ever seen two ladies in the park", "Sipping on ... before it gets dark", "... are they planning to ... next", "Is this life that they're living ...");
 const poem3 = new PoemTemplate(3, "Movement", "Run, run, run", "Run back...", "Or else the things that you ...", "Will only keep ...");
 
-let thePoetThatDidntKnowIt = {
+let thePoetThatDidNotKnowIt = {
     poems: [],
     play() {
-        let selector = Math.ceil(Math.random * 3);
+        const randomPoem = [];
+        let selector = Math.ceil(Math.random() * 3);
+        for (const poem of this.poems) {
+            if(poem.number === selector) {
+                randomPoem.push(poem)
+                console.log(randomPoem)
+            }
+        }
     }
 }
 
-thePoetThatDidntKnowIt.poems.push(poem1, poem2, poem3);
-console.log(thePoetThatDidntKnowIt)
+thePoetThatDidNotKnowIt.poems.push(poem1, poem2, poem3);
+console.log(thePoetThatDidNotKnowIt.play())
 
 // Welcome to The Poet That Didn't Know It!
 
